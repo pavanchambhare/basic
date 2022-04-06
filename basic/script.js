76a04df48f8b5412dd84ebe142db7a35ca824c86
pavanchambhare <pavanchambhare88gmail.com>
2022-04-06 19:14:05 +0530

    first commit

---
+++ basic/script.js
@@ -0,0 +1,43 @@
+var rohit = [0,2,4,6,8]
+ console.log(rohit)
+var mohit = [0,2,4,6,9]
+ console.log(mohit)
+
+ let value = rohit.indexOf(23);
+  console.log(rohit)
+
+var city = 'delhi';
+console.log(city)
+var city = 'moba';
+console.log(city)
+var city = 'kolapur';
+city = 'malkapur';
+console.log(city)
+var city = 'hello';
+console.log(city)
+
+const college = 'malkapur';
+const villege = 'mokgo';
+console.log(villege)
+const myvi = 'malkapur 1';
+console.log(myvi)
+
+var x =  4;
+var y = 15;
+var z = x + y;
+document.getElementById("demo").innerHTML = "the value of z is: " + z;
+console.log(z)
+
+const price1 = 10;
+const price2 = 15;
+let total = price1 + price2;
+document.getElementById("demo1").innerHTML = "the total is:" + total;
+console.log(total)
+
+
+document.getElementById("btn").addEventListener("click", displayDate);
+function displayDate() {
+    document.getElementById("ok").innerHTML = Date();
+}
+
+
